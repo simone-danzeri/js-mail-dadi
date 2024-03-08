5b5d2457cd89966dfcfdd407a5f4bbe85b2f94d7
simone-danzeri <s.danzeri@icloud.com>
2024-03-08 13:33:15 +0100

    feat: DADI - created random numbers for user and computer

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// DADI
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+let userNumber
+let computerNumber
+
+userNumber = Math.floor(Math.random() * 6) + 1;
+computerNumber = Math.floor(Math.random() * 6) + 1;
+console.log(userNumber);
+console.log(computerNumber);
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
